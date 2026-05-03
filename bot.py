6d5300f94efa75ebb2e88643beb063c33e78e64b
heartache7 <abdulazizmaxmudov4405@gmail.com>
2026-05-03 15:15:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from vkbottle.bot import Bot, Message
 
 # ВСТАВЬ СЮДА СВОЙ ТОКЕН
-bot = Bot(token="vk1.a.BjbUku4f6oA9OCTqULJsZtbJBtmW7-b4irA6pB9W51yMYdscwVQhN7_VYnZL1xTBz6gJERKpKCoyzeAvY5K66LPd3zM-uYOUZ8DXakhS_1CWSEbnRUWPctihcbjk5zSxLkEELlL1qg64W4WY6YwlNJG138FkWqL2yQgQbCL2Z-ELrJHiR-46mo9tdAIUOZZRTKZ54jJGKnM4oKGHhxvXEw")
-
+bot = Bot(token="vk1.a.ZTiuYTvGy_O_v5C851uFyvh5vnHrJcoOzvE6dOUdqxEsicPpfKDrS7c8-GthxyBhM9BhGD9es6WtBalvFI89AWm-ME75iWsEdg5JoHyyU20uuT4LrIQnxyS0vJiS2SPF4RPwsSxdtekCBn0wYRV0H2lT0kyQYwwmTXpta7UUfSzaDafRjXuw1Pdpih6EaSC0FEHTVRKYN_IniAii_eSO-w")
 @bot.on.message()
 async def handler(message: Message):
     await message.answer("Бот работает 24/7 🚀")
